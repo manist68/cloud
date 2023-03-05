cd0fa1fbbd8b6cbe39d0e817d29a3fed00ca61c5
manist68 <120625434+manist68@users.noreply.github.com>
2023-03-05 11:07:07 +0530

    Update main.tf

--- nt/main.tf
+++ nt/main.tf
@@ -18,7 +18,7 @@ terraform {
         backend "azurerm" {   
         resource_group_name  = "nt-poc-akshaya"
         storage_account_name = "sinkstrgadf" 
-        container_name       = "terra" 
+        container_name       = "cloud" 
         key                  = "nt/terraform.tfstate" 
     } 
 } 
@@ -28,7 +28,7 @@ data "terraform_remote_state" "rg" {
   config = {
         resource_group_name  = "nt-poc-akshaya"
         storage_account_name = "sinkstrgadf" 
-        container_name       = "terra"  
+        container_name       = "cloud"  
         key                  = "rg/terraform.tfstate"  
   }
 }
